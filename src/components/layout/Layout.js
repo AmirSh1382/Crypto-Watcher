3d53c719ebb80b2801d347ee5739a81c136abda0
AmirSh <amirsh.webdev@gmail.com>
2022-10-20 14:26:21 +0330

    bug fixed

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -7,9 +7,6 @@ import Footer from "./Footer";
 // MUI Icons
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-// Styles
-import styles from "./ScrollBar.module.css"
-
 const Layout = ({ children }) => {
   const [scrollY, setScrollY] = useState(0);
 
@@ -25,7 +22,7 @@ const Layout = ({ children }) => {
   })
 
   return (
-    <div style={{minHeight: `${minHeight}`}} className={`${styles.scrollBar} flex flex-col`}>
+    <div style={{minHeight: `${minHeight}`}} className='flex flex-col'>
       <Header />
       {children}
       <Footer />
